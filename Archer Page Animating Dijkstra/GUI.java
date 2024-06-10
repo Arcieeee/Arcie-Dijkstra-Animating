202b6599c54e7413f717aa105e39b619e8b0d88d
pagear <pagear@whs.school.nz>
2024-06-10 13:18:22 +1200

    Attempted (Failed) to use GUI to start algorithm instance as intended

--- Archer Page Animating Dijkstra/GUI.java
+++ Archer Page Animating Dijkstra/GUI.java
@@ -30,9 +30,9 @@ public class Gui extends JFrame implements ActionListener,MouseListener
         menuBar=new JMenuBar();
         this.setJMenuBar(menuBar);
         
-        menu = new JMenu("A Menu");
+        menu = new JMenu("Algorithm menu");
         menuBar.add(menu);
-        menuItem=new JMenuItem("First menu item");
+        menuItem=new JMenuItem("Run Algorithm");
         menuItem.addActionListener(this);
         menu.add(menuItem);
         
@@ -107,6 +107,7 @@ public class Gui extends JFrame implements ActionListener,MouseListener
          case "MC MENTAL @ HIS BEST" : System.out.println("Bidi bidi bah Bidi bringing that pressure up And the mad kid coming on on at you Poh poh poh poh Bringing it down Widi bidi bi bidi bah Worldstar Now widi bidi bi widi bidi bi Widi bidi bi bi bi bi bass Wah bi di bah, kill the bass Wi rafi rocking off his face, oh Rap party popping MC Mental taking this one away Wicked Bidididididididididididididiididididididididiididididididid Move it all Wididididididididididididididididi bi Widibibibibibibibibibibibiibibbiib dayshawn Dibidibidibidibidibi bar bass Wah buduh wah Kill the bass Wir-afin-afin-afin off his face, nah Ged udududugeludududugeludududugel Udududugeludududugeludududugeludududugel Coming all down Git git git git git git git Give me that fucking bat Coming at you Widibidibidibi Big big bass about to kick Wreck it Wreck it Shawn shawn Wapboh wapboh Windows sign Wapboh Wings are popping Drop it Kick it up and down Rapper MC Mental with the Wick-ki-ki-ked, wickedest sound With the wickedest sound With the wickedest pressure Bigibiduh barn Me warnin bisches Bigibiduh barn Big T letter Kick to the beat Now coming at you Divine Give shag [?] Bringing it fucking down Grab my team Rocking in the place wichukimine One time Rocking with that widibarmine Now Widuhmuduh widumuduh widumuduh widumuduh kick Rohmidibah bidibi di beat now Drop to the beat Kick kick now beat Nah kick nah beat To the beat to top Nah rocking what I want Rapper MC 20 years Now kick to the beat to the max DIVINE Right I'll take you to your fucking place Wicked Back to back with the MC Kronik Right I tell yer Little fucking daisy Yer little pussy Hahahaha One fucking time Right this is the MC Mental fucking session Divine Right I tell you, little fucking Keegan and his clan Fucking ear Mental time");     
               createDialogExample();
               break;
+         case "Run Algorithm" : Algorithm Algorithm = new Algorithm();
          default : System.out.println(cmd+" is better than the other menu item...");
        }
     }
