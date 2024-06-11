14c9e8381eb46ccdc91829f160f7b34cc32a3e8e
pagear <pagear@whs.school.nz>
2024-06-11 16:53:58 +1200

    Code (hopefully) no longer requires info of connected nodes to be stored at that nodes identity in the connections array

--- Archer Page Animating Dijkstra/DistanceChecker.java
+++ Archer Page Animating Dijkstra/DistanceChecker.java
@@ -22,17 +22,17 @@ public class DistanceChecker
         Current = Start; //Sets the Current searched node to the start
         int CurrentDistance = 0; //initialize Distance
         int[][] IdentityDistance = new int[End.Identity+1][3]; //Creates an array as many nodes long with 2 rows, one for checking if a node is already visited, the other for distance
-       
-        for(int h=1; h<End.Identity+1; h++){
-            IdentityDistance[h][0] = 0;
-            IdentityDistance[h][1] = 2147483647; //sets everything in row 1 to 0; row 2 and 3 to max int.
-            IdentityDistance[h][2] = 2147483647;
+        int unvisited=0; int visited=1; int searched=2; int Status=0; int SearchDistance=1; int TrueDistance=2;
+        
+        for(int i=1; i<End.Identity+1; i++){
+            IdentityDistance[i][Status] = 0;
+            IdentityDistance[i][SearchDistance] = 2147483647; //sets everything in row 1 to 0; row 2 and 3 to max int.
+            IdentityDistance[i][TrueDistance] = 2147483647;
         }
         
         IdentityDistance[0][1] = 0; IdentityDistance[0][2] = 0; //except for the start
        
         int i=0; //Counter = 0
-        int unvisited=0; int visited=1; int searched=2; int status=0; int SearchDistance=1; int TrueDistance=2;
         //at this point we have an array full of 0s and max int, and a start distance 0. 
         
           while(Current != End){ //While we are not at the end
@@ -40,14 +40,14 @@ public class DistanceChecker
            
           int x=0;    
                while(Current.Connections[i]==null&&x==0){ //Goes all the connections of the current node until it finds one, or until it runs out of connections to search
-               i++; if (i>End.Identity){i=End.Identity; x=1;}
+               i++; if (i>Current.Connections.length){i=Current.Connections.length; x=1;}
           }
            
            if(x==0){ //if we have a connection
          
            if(IdentityDistance[Current.Identity][SearchDistance] + Current.Connections[i].distance<IdentityDistance[Current.Connections[i].Identity][TrueDistance]){ //if the Search Distance to the node is less than the True Distance of the node
                 
-                  IdentityDistance[Current.Connections[i].Identity][status] = visited; //Set node status to visited
+                  IdentityDistance[Current.Connections[i].Identity][Status] = visited; //Set node Status to visited
                   IdentityDistance[Current.Connections[i].Identity][SearchDistance] = IdentityDistance[Current.Identity][SearchDistance] + Current.Connections[i].distance; //Update Search Distance
                   IdentityDistance[Current.Connections[i].Identity][TrueDistance] = IdentityDistance[Current.Connections[i].Identity][SearchDistance]; //Set True Distance to Search Distance
                   
@@ -59,19 +59,19 @@ public class DistanceChecker
           }
           
           //At this point, we have evaluated all connections of the current Node.
-          IdentityDistance[Current.Identity][1]=2147483647; //sets distance to current node to max int to indicate it has been searched.
-          IdentityDistance[Current.Identity][0]=2;
+          IdentityDistance[Current.Identity][SearchDistance]=2147483647; //sets distance to current node to max int to indicate it has been searched.
+          IdentityDistance[Current.Identity][Status]=searched;
           int x=0;
           
           for(i=0; i<=End.Identity; i++){
               if(IdentityDistance[i][SearchDistance]<=CurrentDistance){
-                  x=i; CurrentDistance=IdentityDistance[i][1];}}
+                  x=i; CurrentDistance=IdentityDistance[i][SearchDistance];}}
           
           if (x!=End.Identity) //if we are not at the end
           {Current = Algorithm.ArrayNodes[x];} 
           else {Current = End;} //Current = Node with that identity.
           
-          i=1;
+          i=0;
          }
         return CurrentDistance;
     }
