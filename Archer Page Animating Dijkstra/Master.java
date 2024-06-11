329d4db7669b7cb19ac8616e3a2ac517a07640db
pagear <pagear@whs.school.nz>
2024-06-12 11:08:26 +1200

    Last changes for the day
    Nothing notable.

--- Archer Page Animating Dijkstra/Master.java
+++ Archer Page Animating Dijkstra/Master.java
@@ -8,26 +8,5 @@
 public class Master
 {
     // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Master
-     */
-    public Master()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    Gui Gui = new Gui(1600, 800, "");
 }
