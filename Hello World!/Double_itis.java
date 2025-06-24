dfb2f1085bd0d2aaea8ec5b35c8917568e0026b6
Arcieeee <pagear@whs.school.nz>
2025-06-25 01:13:07 +1200

    Delete Hello World! directory

--- Hello World!/Double_itis.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Double_itis {
-    
-    public static void main(String[] args){
-      Scanner input = new Scanner(System.in);
-      int x=input.nextInt();
-      if(x/2>=input.nextInt()){System.out.print("Yes");} else {System.out.print("No");}
-    }
-}
--- Hello World!/Template.java
+++
@@ -1,6 +0,0 @@
-import java.util.Scanner;
-public class Template { //Replace template with Main when submitting
-    public static void main(String[] args){
-     Scanner input = new Scanner(System.in);
-    }
-}
